1df6f922adb1d87e4efeba9a81b9a7e3d9fc1ed7
陈典渠 <1342439191@qq.com>
2019-06-27 16:10:00 +0800

    netty聊天室初始化

---
+++ chat_netty4/common/src/main/java/com/cn/common/core/codc/ConstantValue.java
@@ -0,0 +1,13 @@
+package com.cn.common.core.codc;
+/**
+ * 静态值
+ * @author laofeizhu
+ *
+ */
+public interface ConstantValue {
+	/**
+	 * 包头标识
+	 */
+	public static int HEADER_FLAG = -21415431;
+}
+                                                  
---
+++ chat_netty4/server/src/main/java/com/cn/server/module/chat/handler/ChatHandler.java
@@ -0,0 +1,40 @@
+package com.cn.server.module.chat.handler;
+
+import com.cn.common.core.annotion.SocketCommand;
+import com.cn.common.core.annotion.SocketModule;
+import com.cn.common.core.model.Result;
+import com.cn.common.module.ModuleId;
+import com.cn.common.module.chat.ChatCmd;
+import com.cn.common.module.chat.request.PrivateChatRequest;
+import com.cn.common.module.chat.request.PublicChatRequest;
+
+/**
+ * 聊天
+ *
+ * @author laofeizhu
+ */
+@SocketModule(module = ModuleId.CHAT)
+public interface ChatHandler {
+
+
+    /**
+     * 广播消息
+     *
+     * @param playerId
+     * @param data     {@link PublicChatRequest}
+     * @return
+     */
+    @SocketCommand(cmd = ChatCmd.PUBLIC_CHAT)
+    Result<?> publicChat(long playerId, byte[] data);
+
+
+    /**
+     * 私人消息
+     *
+     * @param playerId
+     * @param data     {@link PrivateChatRequest}
+     * @return
+     */
+    @SocketCommand(cmd = ChatCmd.PRIVATE_CHAT)
+    Result<?> privateChat(long playerId, byte[] data);
+}
